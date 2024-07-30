abe61f1b76af7fcf53fb046f635f46fa9cc50bfa
Harrington-23 <aaron.harrington23@gmail.com>
2024-07-30 17:47:28 +0100

    Complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+  let original = word;
+  let reversed = original.split('').reverse().join('');
+  return reversed;
 };
 
 // Do not edit below this line
